e2dcfd8c66d4002bb192447de72c9b612ec951c7
Sven Ansem <sven.ansem@sogeti.nl>
2016-08-09 14:34:25 +0200

    Makes it easier to trach DB changes

---
+++ src/Nop.Database/FullTextIndexes.sql
@@ -0,0 +1,19 @@
+﻿CREATE FULLTEXT INDEX ON [dbo].[Product]
+    ([Name] LANGUAGE 1033, [ShortDescription] LANGUAGE 1033, [FullDescription] LANGUAGE 1033)
+    KEY INDEX [PK__Product__3214EC076842AB5D]
+    ON [nopCommerceFullTextCatalog];
+
+
+GO
+CREATE FULLTEXT INDEX ON [dbo].[ProductTag]
+    ([Name] LANGUAGE 1033)
+    KEY INDEX [PK__ProductT__3214EC07D184BA52]
+    ON [nopCommerceFullTextCatalog];
+
+
+GO
+CREATE FULLTEXT INDEX ON [dbo].[LocalizedProperty]
+    ([LocaleValue] LANGUAGE 1033)
+    KEY INDEX [PK__Localize__3214EC077B9F28D6]
+    ON [nopCommerceFullTextCatalog];
+
---
+++ src/Nop.Database/Storage/nopCommerceFullTextCatalog.sql
@@ -0,0 +1,5 @@
+﻿CREATE FULLTEXT CATALOG [nopCommerceFullTextCatalog]
+    WITH ACCENT_SENSITIVITY = ON
+    AS DEFAULT
+    AUTHORIZATION [dbo];
+
